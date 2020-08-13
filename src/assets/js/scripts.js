5735ccb81b9dbc5c9e1ccd039be87bfcb01d9893
mioriaty <mioriaty@gmail.com>
2020-08-13 16:52:15 +0700

    add: mega menu(demo)

--- src/assets/js/scripts.js
+++ src/assets/js/scripts.js
@@ -1,8 +1,10 @@
 (function () {
   "use-strict";
   // javascript
-  const btnToggle = document.querySelector(".toggle-bar");
+  const btnToggleBar = document.querySelector(".toggle-bar");
   const headerMenu = document.querySelector(".header__menu");
+  const btnToggleMega = document.querySelector(".has-megamenu");
+  const megaMenu = document.querySelector(".mega-menu");
 
   const handleToggle = () => {
     headerMenu.classList.toggle("is-hidden");
@@ -24,7 +26,22 @@
     }
   };
 
+  const handleDisplayMegaMenu = (e) => {
+    e.preventDefault();
+    const megaMenuStyles = {
+      opacity: 1,
+      visibility: "visible",
+      position: "relative",
+      top: "unset",
+      left: "unset",
+    };
+    console.log(megaMenuStyles);
+    Object.assign(megaMenu.style, megaMenuStyles);
+  };
+
   renderDisplayMenu();
-  btnToggle.addEventListener("click", handleToggle);
+  btnToggleBar.addEventListener("click", handleToggle);
   window.addEventListener("resize", handleResize);
+  btnToggleMega.addEventListener("click", handleDisplayMegaMenu);
+  btnToggleMega.addEventListener("onhover", handleDisplayMegaMenu);
 })();
