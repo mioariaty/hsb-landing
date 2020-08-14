088af537848311f9d86705b37bc0cb359b53339f
mioriaty <mioriaty@gmail.com>
2020-08-14 15:09:47 +0700

    Add: mixin setPosition, setIconSize"

--- src/assets/js/scripts.js
+++ src/assets/js/scripts.js
@@ -4,7 +4,7 @@
   const btnToggleBar = document.querySelector(".toggle-bar");
   const headerMenu = document.querySelector(".header__menu");
   const btnToggleMega = document.querySelectorAll(".has-megamenu");
-  const megaMenu = document.querySelector(".mega-menu");
+  const megaMenu = document.querySelectorAll(".mega-menu");
 
   const handleToggle = () => headerMenu.classList.toggle("is-hidden");
 
@@ -24,20 +24,27 @@
     }
   };
 
-  const toggleDisplayMegaMenu = (e) => {
-    console.log(e);
-    e.preventDefault();
-    if (window.innerWidth <= 1023) {
-      megaMenu.classList.toggle("is-mega-shown");
-    } else {
-      megaMenu.classList.toggle("is-shown");
-    }
-  };
+  // const toggleDisplayMegaMenu = (e) => {
+  //   e.preventDefault();
+  //   const target = e.target;
+
+  //   const menu = target.nextElementSibling;
+  //   // // menu.classList.add("is-shown");
+  //   // console.log(menu);
+  //   // console.log(target.nextElementSibling);
+  //   if (window.innerWidth <= 1023) {
+  //     // item.classList.toggle("is-mega-shown")
+  //     menu.classList.toggle("is-mega-shown");
+  //   } else {
+  //     // item.classList.toggle("is-shown")
+  //     menu.classList.toggle("is-shown");
+  //   }
+  // };
 
   renderDisplayMenu();
   btnToggleBar.addEventListener("click", handleToggle);
   window.addEventListener("resize", handleResize);
   btnToggleMega.forEach((item) =>
-    item.addEventListener("click", toggleDisplayMegaMenu)
+    item.addEventListener("click", (e) => e.preventDefault())
   );
 })();
